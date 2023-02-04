1d1ab728b993a40f5fa2cd1644471f05a8b54f3e
md.farid <md.farid.ice@gmail.com>
2023-02-04 12:19:22 +0600

    first approch to create user in memory

--- src/main/java/com/teamwebsoft/config/ProjectSecurityConfig.java
+++ src/main/java/com/teamwebsoft/config/ProjectSecurityConfig.java
@@ -3,6 +3,9 @@ package com.teamwebsoft.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,5 +23,23 @@ public class ProjectSecurityConfig {
                 .and().formLogin()
                 .and().httpBasic();
         return http.build();
+    }
+
+    @Bean
+    InMemoryUserDetailsManager userDetailsService(){
+        /*Approach 1 where we use withDefaultPasswordEncoder() method
+        while creating the user details*/
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("12345")
+                .authorities("admin")
+                .build();
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("12345")
+                .authorities("read")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin,user);
     }
 }
